f08c1fd691951ae4de230a25307057ee657101a6
NicolaiTue <nicolai.tue@gmail.com>
2023-10-27 09:54:18 +0200

    Start på network

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,30 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        print("connecting to server");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("connected to server");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconected from server. Reason" + cause.ToString());
+    }
+
+
+
+
+}
